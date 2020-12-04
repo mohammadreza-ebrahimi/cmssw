9144dd6c071a72a88e63ca4054e30d13b99258b7
Ianna Osborne <ianna.osborne@cern.ch>
2020-12-04 13:24:32 +0100

    backport of 32339 and 32371 to assure thread-safe dd4hep workflow

--- DetectorDescription/DDCMS/plugins/test/DDTestSpecParsFilter.cc
+++ DetectorDescription/DDCMS/plugins/test/DDTestSpecParsFilter.cc
@@ -13,6 +13,12 @@ using namespace std;
 using namespace cms;
 using namespace edm;
 
+#ifdef HAVE_GEANT4_UNITS
+#define MM_2_CM 1.0
+#else
+#define MM_2_CM 0.1
+#endif
+
 class DDTestSpecParsFilter : public one::EDAnalyzer<> {
 public:
   explicit DDTestSpecParsFilter(const ParameterSet& iConfig)
@@ -67,7 +73,7 @@ void DDTestSpecParsFilter::analyze(const Event&, const EventSetup& iEventSetup) 
       for (const auto& kl : t.second->numpars) {
         log << kl.first << " = ";
         for (const auto& kil : kl.second) {
-          log << kil << " ";
+          log << kil / MM_2_CM << " ";
         }
         log << "\n ";
       }
