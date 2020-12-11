4e6cdf882a50ac2c8c5f81c8bc3218a845324c46
Chris West <christopher.alan.west@gmail.com>
2020-12-11 20:22:07 +0100

    New JEC for 2018 HI re-miniAOD

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -45,6 +45,13 @@ autoCond = {
     'run3_data'                    : '124X_dataRun3_v14',
     # GlobalTag for Run3 data relvals: allows customization to run with fixed L1 menu - snapshot at 2022-12-15 07:28:44 (UTC)
     'run3_data_relval'             : '125X_dataRun3_relval_v6',
+    'run2_hlt_relval_hi'   :   '103X_dataRun2_HLT_relval_HI_v5',
+    # GlobalTag for Run2 HLT for HI (not 2018 HI): it points to the online GT
+    'run2_hlt_hi'       :   '101X_dataRun2_HLTHI_frozen_v10',
+    # GlobalTag for Run3 data relvals (express GT)
+    'run3_data_express'        :   '111X_dataRun3_Express_v4',
+    # GlobalTag for Run3 data relvals
+    'run3_data_promptlike'     :   '111X_dataRun3_Prompt_v4',
     # GlobalTag for MC production with perfectly aligned and calibrated detector for Phase1 2017 (and 0,0,~0-centred beamspot)
     'phase1_2017_design'           : '123X_mc2017_design_v2',
     # GlobalTag for MC production with realistic conditions for Phase1 2017 detector
